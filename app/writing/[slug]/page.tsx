c8f069c99d6b4e6447d150b6fc8788774a1f7ffb
Ching Lam Lau <contact.minicube11@gmail.com>
2023-05-12 17:38:42 -0400

    Add back link for post

--- app/writing/[slug]/page.tsx
+++ app/writing/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import Markdown from "markdown-to-jsx";
 import getPostContent from "@/lib/posts/getPostContent";
 import getPostMetaData from "@/lib/posts/getPostMetadata";
+import Link from "next/link";
 
 export const generateStaticParams =async () => {
     const posts = getPostMetaData();
@@ -13,12 +14,15 @@ const PostPage = (props: any) => {
     const slug = props.params.slug;
     const post = getPostContent(slug);
     return (
-        <div className="mt-20 mx-auto max-w-2xl">
+        <div className="mt-20 mb-20 mx-auto max-w-2xl">
             <h1 className="mb-6">{post.data.title}</h1>
             <p className="text-gray-700 mb-14 dark:text-gray-400">{post.data.date}<span className="ml-8">Ching Lam Lau</span></p>
             <div className="post-content mb-20">
                 <Markdown>{post.content}</Markdown>
             </div>
+            <Link href="/writing">
+                ← Back to all posts
+            </Link>
         </div>
     )
 };
