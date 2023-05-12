3b06245be673f25c7e6cf415ac92f26878635c1d
Ching Lam Lau <contact.minicube11@gmail.com>
2023-05-12 18:13:18 -0400

    Update contact link styling on about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,21 +1,18 @@
+import ContactLink from "@/components/About/ContactLink";
 import Link from "next/link";
 
 const About = () => {
     return (
-        <div className="mt-20 mx-auto max-w-2xl p-4 space-y-16">
+        <div className="my-20 mx-auto max-w-2xl p-4 space-y-16">
             <h2>About</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus numquam dolore illo ex necessitatibus qui mollitia. Consequuntur quisquam veritatis ratione obcaecati cum ut quos, inventore, ipsam, quibusdam architecto quae id. Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat distinctio suscipit unde ab odio cumque quaerat repudiandae iste quas ipsum, dolorem temporibus ipsam consectetur maiores sunt repellendus, officia deserunt? Temporibus.</p>
 
             <div className="mt-16">
-                <Link href="mailto:contact.minicube11@gmail.com">Email</Link>
-                <span className="mx-3 text-gray-500">|</span>
-                <Link href="https://github.com/MiniCube11">Github</Link>
-                <span className="mx-3 text-gray-500">|</span>
-                <Link href="https://www.linkedin.com/in/ching-lam-lau/">Linkedin</Link>
-                <span className="mx-3 text-gray-500">|</span>
-                <Link href="https://dmoj.ca/user/MiniCube11">DMOJ</Link>
-                <span className="mx-3 text-gray-500">|</span>
-                <Link href="https://devpost.com/MiniCube11">Devpost</Link>
+                <ContactLink website="Github" handle="MiniCube11" link="https://github.com/MiniCube11"/>
+                <ContactLink website="Linkedin" handle="ching-lam-lau" link="https://www.linkedin.com/in/ching-lam-lau/"/>
+                <ContactLink website="DMOJ" handle="MiniCube11" link="https://dmoj.ca/user/MiniCube11"/>
+                <ContactLink website="Devpost" handle="MiniCube11" link="https://devpost.com/MiniCube11"/>
+                <ContactLink website="Email" handle="contact.minicube11@gmail.com" link="mailto:contact.minicube11@gmail.com"/>
             </div>
         </div>
     )
---
+++ components/About/ContactLink.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const ContactLink = ({ website = "", handle = "", link = "" }) => {
+    return (
+        <div className="mb-6">
+            <p className="text-gray-500 dark:text-gray-400">{website}</p>
+            <Link href={link} target="_blank" rel="noopener noreferrer">{handle} ↗</Link>
+        </div>
+    )
+};
+
+export default ContactLink;
