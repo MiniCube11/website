bb7f6ca28f4d1170a95b2efb916a7fcf53e1dbeb
Ching Lam Lau <contact.minicube11@gmail.com>
2023-05-12 17:13:57 -0400

    Use next-themes for dark mode

--- components/Sidebar/DarkModeIcon.tsx
+++ components/Sidebar/DarkModeIcon.tsx
@@ -1,22 +1,29 @@
 import { useState } from "react";
+import { useTheme } from "next-themes";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMoon } from "@fortawesome/free-solid-svg-icons";
 
 const DarkModeIcon = () => {
     const [hovered, setHovered] = useState(false);
+    const { theme, setTheme } = useTheme();
+
+    const toggleTheme = () => {
+        setTheme(theme == "light" ? "dark" : "light");
+    }
 
     return (
-        <a
+        <span
+            onClick={toggleTheme}
             className="block p-3 w-12 h-12 mx-2 text-center rounded-md text-xl text-gray-500"
             onMouseOver={() => setHovered(true)}
             onMouseOut={() => setHovered(false)}
         >
             <FontAwesomeIcon icon={faMoon} />
             {hovered &&
-                <span className="absolute ml-7 text-gray-800 text-base">Night</span>
+                <span className="absolute ml-7 text-gray-800 text-base dark:text-red-500">Night</span>
             }
-        </a>
+        </span>
     )
 };
 
