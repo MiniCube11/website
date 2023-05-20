01349116c23b31982871a94d825a5d3531a7aee1
Ching Lam Lau <contact.minicube11@gmail.com>
2023-05-20 10:00:13 -0400

    Update dark mode icon styling

--- components/Sidebar/DarkModeIcon.tsx
+++ components/Sidebar/DarkModeIcon.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useTheme } from "next-themes";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMoon } from "@fortawesome/free-solid-svg-icons";
+import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
 
 const DarkModeIcon = () => {
     const [hovered, setHovered] = useState(false);
@@ -15,11 +15,11 @@ const DarkModeIcon = () => {
     return (
         <span
             onClick={toggleTheme}
-            className="block p-3 w-12 h-12 mx-2 text-center rounded-md text-xl text-gray-500"
+            className="block p-3 w-12 h-12 mx-2 text-center rounded-md text-gray-500 hover:bg-gray-100 dark:hover:bg-gray-800"
             onMouseOver={() => setHovered(true)}
             onMouseOut={() => setHovered(false)}
         >
-            <FontAwesomeIcon icon={faMoon} />
+            <FontAwesomeIcon icon={theme == "light" ? faMoon : faSun} />
             {hovered &&
                 <span className="absolute ml-7 text-gray-800 text-base dark:text-gray-300">
                     {theme == "light" ? "Dark" : "Light"}
