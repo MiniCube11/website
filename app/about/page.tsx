ca845aafa3d5d9ccba8ea856a238c5396cad1fb3
Ching Lam Lau <contact.minicube11@gmail.com>
2023-05-20 10:38:05 -0400

    Update about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,11 +3,18 @@ import Link from "next/link";
 
 const About = () => {
     return (
-        <div className="my-20 mx-auto max-w-2xl p-4 space-y-16">
+        <div className="my-20 mx-auto max-w-2xl p-4">
             <h2>About</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus numquam dolore illo ex necessitatibus qui mollitia. Consequuntur quisquam veritatis ratione obcaecati cum ut quos, inventore, ipsam, quibusdam architecto quae id. Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat distinctio suscipit unde ab odio cumque quaerat repudiandae iste quas ipsum, dolorem temporibus ipsam consectetur maiores sunt repellendus, officia deserunt? Temporibus.</p>
+            <p className="mt-12">Hey, I'm Ching Lam, a high school student from Waterloo. I love making websites, going to hackathons, and participating in programming contests.</p>
+            <p className="mt-5">
+                Some cool things I've been part of:
+                <ul className="mt-1">
+                    <li>Organizing <Link href="https://mini.xdhacks.com/hackteam6/byte-the-web" target="_blank" rel="noopener noreferrer">Byte the Web ↗</Link></li>
+                    <li>Representing Canada at the European Girls' Olympiad in Informatics (EGOI)</li>
+                </ul>
+            </p>
 
-            <div className="mt-16">
+            <div className="mt-16 grid grid-cols-2">
                 <ContactLink website="Github" handle="MiniCube11" link="https://github.com/MiniCube11"/>
                 <ContactLink website="Linkedin" handle="ching-lam-lau" link="https://www.linkedin.com/in/ching-lam-lau/"/>
                 <ContactLink website="DMOJ" handle="MiniCube11" link="https://dmoj.ca/user/MiniCube11"/>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 const HomePage = () => {
     return (
-        <div className="my-20 mx-auto max-w-2xl p-4 space-y-16">
-            <h1>Hello!</h1>
+        <div className="my-20 mx-auto max-w-2xl p-4">
+            <p className="text-lg text-blue-400">Hi, my name is</p>
+            <h1 className="mt-3">Ching Lam Lau</h1>
         </div>
     )
 };
