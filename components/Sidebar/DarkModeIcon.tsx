2f8d4f0835eda75ebe3249684692e497582b4b94
Ching Lam Lau <contact.minicube11@gmail.com>
2023-05-21 12:44:11 -0400

    Update dark mode icon expanded

--- components/Sidebar/DarkModeIcon.tsx
+++ components/Sidebar/DarkModeIcon.tsx
@@ -13,19 +13,34 @@ const DarkModeIcon = ({ expanded = false }) => {
     }
 
     return (
-        <span
+        <>
+        {expanded &&
+            <span
             onClick={toggleTheme}
-            className="block p-3 w-12 h-12 mx-2 text-center rounded-md text-gray-500 hover:bg-gray-100 dark:hover:bg-gray-800"
+            className="cursor-pointer block py-2 px-3 w-32 h-10 mx-2 rounded-md text-gray-500 hover:bg-gray-100 dark:hover:bg-gray-800"
             onMouseOver={() => setHovered(true)}
             onMouseOut={() => setHovered(false)}
-        >
-            <FontAwesomeIcon icon={theme == "light" ? faMoon : faSun} />
-            {hovered &&
-                <span className="absolute ml-7 text-gray-800 text-base dark:text-gray-300">
-                    {theme == "light" ? "Dark" : "Light"}
-                </span>
-            }
-        </span>
+            >
+                <FontAwesomeIcon icon={theme == "light" ? faMoon : faSun} className="w-6 text-sm" />
+                <span className="ml-4 text-sm text-gray-800 dark:text-gray-300">{theme == "light" ? "Dark" : "Light"}</span>
+            </span>
+        }
+        {!expanded &&
+            <span
+            onClick={toggleTheme}
+            className="cursor-pointer block p-3 w-12 h-12 mx-2 rounded-md text-gray-500 hover:bg-gray-100 dark:hover:bg-gray-800"
+            onMouseOver={() => setHovered(true)}
+            onMouseOut={() => setHovered(false)}
+            >
+                <FontAwesomeIcon icon={theme == "light" ? faMoon : faSun} />
+                {hovered &&
+                    <span className="absolute ml-7 text-sm text-gray-800 text-base dark:text-gray-300">
+                        {theme == "light" ? "Dark" : "Light"}
+                    </span>
+                }
+            </span>
+        }
+        </>
     )
 };
 
