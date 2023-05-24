f2fa270d4daaafc3ca4b7f83a3669464c2387a11
Ching Lam Lau <contact.minicube11@gmail.com>
2023-05-23 21:01:26 -0400

    Add Umami analytics script

--- app/head.tsx
+++ app/head.tsx
@@ -1,9 +1,16 @@
+import Script from "next/script";
+
 export default function Head() {
     return (
         <>
             <title>Ching Lam Lau</title>
             <meta content="width=device-width, initial-scale=1" />
             <link rel="icon" href="/favicon.ico" />
+            <Script
+            src={process.env.NEXT_PUBLIC_UMAMI_SCRIPT_URL}
+            data-website-id={process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID}
+            strategy="lazyOnload"
+          />
         </>
     )
 }
