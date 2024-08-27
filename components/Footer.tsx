9e72e1c373cc8e29315d0534f3ba5e4fc0dfb361
Ching Lam Lau <contact.minicube11@gmail.com>
2024-08-27 14:45:54 -0400

    Update copyright

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer className="text-center text-sm text-gray-700 p-8 dark:text-gray-300">
             <Link href="https://github.com/MiniCube11/website" target="_blank" rel="noopener noreferrer" className="highlight">
-                Designed and developed by Ching Lam Lau © 2023 
+                Designed and developed by Ching Lam Lau © 2024 
             </Link>
         </footer>
     )    
