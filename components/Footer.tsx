8cb8d8b1c677bb9efb88749b9f1e38b40e8983e5
Ching Lam Lau <contact.minicube11@gmail.com>
2024-11-25 13:53:22 -0500

    Fix footer on mobile

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -14,11 +14,12 @@ const Footer = () => {
     }, []);
 
     return (
-        <footer className="flex w-screen justify-center text-center text-sm text-gray-700 p-8 dark:text-gray-300">
-            <Link href="https://github.com/MiniCube11/website" target="_blank" rel="noopener noreferrer" className="highlight mr-2">
-                Designed and developed by Ching Lam Lau © 2024 |
+        <footer className="flex flex-col md:flex-row justify-center items-center text-center text-sm text-gray-700 p-8 dark:text-gray-300">
+            <Link href="https://github.com/MiniCube11/website" target="_blank" rel="noopener noreferrer" className="highlight">
+                Designed and developed by Ching Lam Lau © 2024 
+                <span className="mx-2 hidden md:inline">|</span>
             </Link>
-            <div className="flex items-center gap-[8px]">
+            <div className="flex items-center gap-[8px] mt-2 md:mt-0">
                 <Link href='https://cs.uwatering.com/#https://chinglamlau.vercel.app/?nav=prev' className="highlight">←</Link>
                 <Link href='https://cs.uwatering.com/#https://chinglamlau.vercel.app/' target='_blank' className="highlight">
                     <Image
